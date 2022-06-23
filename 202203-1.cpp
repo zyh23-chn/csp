1eba977a51b36a06b216d82e9ab03120e027a8ed
Farmer <83542187+zyh0024@users.noreply.github.com>
2022-06-23 18:29:12 +0800

    Create 202203-1.cpp

---
+++ 202203-1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    vector<int> a(n + 1);
+    a[0] = 1;
+    int ans = 0;
+    for (int i = 0; i < k; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        ans += 1 - a[y];
+        a[x] = 1;
+    }
+    printf("%d\n", ans);
+}
